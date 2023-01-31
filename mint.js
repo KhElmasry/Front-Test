1070e53530e884e3e2d3ebf0121010ad218c168b
KhElmasry <123646372+KhElmasry@users.noreply.github.com>
2023-01-31 22:18:35 +0200

    Add files via upload

---
+++ mint.js
@@ -0,0 +1,17 @@
+const Web3 = require('web3');
+const web3 = new Web3('4e79e9a2-b4ce-40ab-b3da-0f9459f0068c');
+
+const contractAddress = '0xF8402BE77548A298F6Ef979f5AAd90573cfC99A0';
+const contractABI = [{...}];
+const contract = new web3.eth.Contract(contractABI, contractAddress);
+
+const mintButton = document.getElementById('mintButton');
+const mintCount = document.getElementById('mintCount');
+
+mintButton.addEventListener('click', () => {
+  contract.methods.mint().send({from: web3.eth.defaultAccount})
+    .then(() => {
+      mintCount.innerHTML = parseInt(mintCount.innerHTML) + 1;
+    })
+    .catch(error => {
+      console.
